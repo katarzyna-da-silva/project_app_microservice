99c81963b5d5674f1ac3ea1ff24b48bd5eefe525
katarzyna-da-silva <conic916@gmail.com>
2024-04-18 15:48:15 +0200

    add files

---
+++ terraform/eks-node-group-public.tf
@@ -0,0 +1,93 @@
+# CREATE AWS EKS NODE GROUP - PUBLIC
+# IAM PERMISSIONS, CRTEATED BEFORE AND DELETED AFTER EKS 
+
+resource "aws_eks_node_group" "eks_ng_public" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${local.name}-eks-ng-public"
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
+  subnet_ids      = module.vpc.public_subnets 
+  #version         = var.cluster_version
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
+
+  update_config {
+    max_unavailable = 1
+    # max_unavailable_percentage = 50
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks-AmazonEC2ContainerRegistryReadOnly,
+  ]
+
+tags = {
+  Name = "Public-Node-Group"
+}
+}
+
+
+# 2ieme node : 
+resource "aws_eks_node_group" "eks_ng_public_2" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${local.name}-eks-ng-public-2" 
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
+
+  subnet_ids      = module.vpc.public_subnets
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks-AmazonEC2ContainerRegistryReadOnly,
+  ]
+
+  tags = {
+    Name = "Public-Node-Group-2"
+  }
+}
+
+
+# 2ieme node : 
+resource "aws_eks_node_group" "eks_ng_public_3" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${local.name}-eks-ng-public-3" 
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
+
+  subnet_ids      = module.vpc.public_subnets
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks-AmazonEC2ContainerRegistryReadOnly,
+  ]
+
+  tags = {
+    Name = "Public-Node-Group-3"
+  }
+}
