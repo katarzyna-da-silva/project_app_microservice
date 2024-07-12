46c4435442ceb3ab53a4e15b10bec29a72dde9bc
katarzyna-da-silva <conic916@gmail.com>
2024-07-12 16:54:55 +0200

    add terraform

--- terraform/vpc-modules.tf
+++ terraform/vpc-modules.tf
@@ -4,7 +4,10 @@ data "aws_availability_zones" "available" {
 
 # Create VPC Terraform Module
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"  
+  source  = "terraform-aws-modules/vpc/aws"
+  #version = "3.11.0"
+  #version = "~> 3.11"
+  #version = "4.0.1"  
   version = "5.4.0"  
   
   # VPC Basic Details
@@ -12,23 +15,42 @@ module "vpc" {
   cidr = var.vpc_cidr_block
   azs             = data.aws_availability_zones.available.names
   public_subnets  = var.vpc_public_subnets
-  private_subnets = var.vpc_private_subnets
+  private_subnets = var.vpc_private_subnets  
+
+  # Database Subnets
+  database_subnets = var.vpc_database_subnets
+  create_database_subnet_group = var.vpc_create_database_subnet_group
+  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+  # create_database_internet_gateway_route = true
+  # create_database_nat_gateway_route = true
   
   # NAT Gateways - Outbound Communication
   enable_nat_gateway = var.vpc_enable_nat_gateway 
-  enable_dns_support   = true
-  enable_dns_hostnames   = true
+  single_nat_gateway = var.vpc_single_nat_gateway
 
-  tags = {
-    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"
+  # VPC DNS Parameters
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  
+  tags = local.common_tags
+  vpc_tags = local.common_tags
+
+  # Additional Tags to Subnets
+  public_subnet_tags = {
+    Type = "Public Subnets"
+    "kubernetes.io/role/elb" = 1    
+    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"        
+  }
+  private_subnet_tags = {
+    Type = "private-subnets"
+    "kubernetes.io/role/internal-elb" = 1    
+    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"    
   }
 
- # Instances launched into the Public subnet should be assigned a public IP address.
+  database_subnet_tags = {
+    Type = "database-subnets"
+  }
+  # Instances launched into the Public subnet should be assigned a public IP address.
   map_public_ip_on_launch = true
 }
-#  private_subnet_tags = {
- #   Type = "private-subnets"
-  #  "kubernetes.io/role/internal-elb" = 1"   
-   # "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"    
- # }
- 
