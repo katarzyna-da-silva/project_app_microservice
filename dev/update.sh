b79100ac8da86f1b43b5da92c0c1b199252072c8
katarzyna-da-silva <conic916@gmail.com>
2024-07-12 16:58:22 +0200

    add file sh

--- dev/update.sh
+++ dev/update.sh
@@ -5,7 +5,7 @@ TERRAFORM_DIR="/home/z390/Bureau/project_app_microservice/dev/terraform/"
 echo "Updating system"
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-cd "$TERRAFORM_DIR" || { echo "Terraform directory not found"; exit 1; }
+cd "$TERRAFORM_DIR" || { echo "Not found"; exit 1; }
 
 echo "Starting Terraform"
 terraform plan  ## apply -auto-approve
